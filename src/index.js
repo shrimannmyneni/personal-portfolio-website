a3319d36041f9bf3cf436d38ac84f61c48e0de7c
shrimannmyneni <shrimann@umich.edu>
2025-01-18 11:24:21 -0500

    build

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/personal-portfolio-project">
+    <BrowserRouter basename="/personal-portfolio-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
